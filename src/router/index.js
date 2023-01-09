bfdab1f52781baae31f877843c78a4efdfba9dcf
Thalyson Barros <thalysonbarrospereira2019@gmail.com>
2023-01-09 17:47:06 -0300

    CHORE: Finalizando a implementação do e-commerce

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHashHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import BasketView from '../views/BasketView.vue';
 
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'HomeView',
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/basket',
+    name: 'BasketView',
+    component: BasketView
   }
-]
+];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(),
   routes
-})
+});
 
-export default router
+export default router;
